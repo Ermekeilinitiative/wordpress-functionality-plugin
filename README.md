ebc5209f48c8f5889121acf047cddaca1aa9c359
Daniel Appelt <daniel.appelt@gmail.com>
2015-12-21 23:05:22 +0100

    Register a global menu that may only be customized from the main site.

--- README.md
+++ README.md
@@ -10,5 +10,8 @@ This plugin should be activated for the whole multisite network in order to make
 
 ## Changelog
 
+### 0.2
+* Register a global menu across the network which may only be customized from the main site.
+
 ### 0.1
 * Use a separate theme template for the network's front page.
--- ermekeilkarree-functionality-plugin.php
+++ ermekeilkarree-functionality-plugin.php
@@ -2,13 +2,46 @@
 /*
    Plugin Name: Ermekeilkarree Functionality Plugin
    Description: Plugin for site ermekeilkarree.de offering functionality that is independent from themes. Activate this plugin for the whole multisite network to make at an integral part of the network.
-   Version: 0.1
+   Version: 0.2
    License: MIT
    Author: Daniel Appelt
    Author URI: https://github.com/danielappelt
    See also: http://wpcandy.com/teaches/how-to-create-a-functionality-plugin/
-
  */
+
+/* 
+   Register a global menu across the network.
+
+   See http://wpmututorials.com/plugins/add-a-global-menu-to-your-network/
+ */
+register_nav_menu( 'global', 'Global Navigation Menu' );
+
+function global_nav_menu_init() {
+    if (!is_main_site()) {
+        // Remove the menu from menu screens on sub sites to avoid confusions.
+        unregister_nav_menu('global');
+    }
+}
+// Remove the global menu from admin and customizing areas in sub sites.
+add_action('admin_init', 'global_nav_menu_init');
+add_action('start_previewing_theme', 'global_nav_menu_init');
+
+function global_menu_filter($output, $args) {
+    // This is the current network's information; 'site' is old terminology.
+    global $current_site;
+
+    $sorted_menu_items = null;
+    if(!is_main_site() && $args->theme_location == "global") {
+        // Retrieve the main site's global menu.
+        switch_to_blog($current_site->blog_id);
+        $sorted_menu_items = wp_nav_menu($args);
+        restore_current_blog();
+    }
+
+    return $sorted_menu_items;
+}
+// See http://codex.wordpress.org/Function_Reference/add_filter for priority and accepted_args.
+add_filter('pre_wp_nav_menu', 'global_menu_filter', 10, 2);
 
 // Use template multisite_front_page.php if available to display the network's front page!
 function front_page_filter($template) {
